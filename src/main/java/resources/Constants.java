b58e2ee835cf9da030f554da6776c100e06f47f1
LORDexile <exilexxxx@i.ua>
2016-11-26 03:54:18 +0200

    ver 0.0.1a

--- src/main/java/resources/Constants.java
+++ src/main/java/resources/Constants.java
@@ -1,6 +1,6 @@
 package resources;
 
 public final class Constants {
-	public static final String PRODUCT_VERSION = "0.0.1";
-	public static final String PROGRAM_TITLE = " ScreenShotSaver - SSS ver: [" + PRODUCT_VERSION + "]";
+	public static final String PRODUCT_VERSION = "0.0.1a";
+	public static final String PROGRAM_TITLE = " ScreenShotSaver ver: [" + PRODUCT_VERSION + "]";
 }
