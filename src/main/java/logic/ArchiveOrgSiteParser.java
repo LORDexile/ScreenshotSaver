eb6f0784af330c38a57805eb2bc6aebe84c1dfd2
LORDexile <exilexxxx@i.ua>
2016-11-30 02:20:59 +0200

    set maximum urls per Year

--- src/main/java/logic/ArchiveOrgSiteParser.java
+++ src/main/java/logic/ArchiveOrgSiteParser.java
@@ -1,8 +1,8 @@
 package logic;
+
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
@@ -18,11 +18,16 @@ public class ArchiveOrgSiteParser implements SiteParser, SiteParserExtend {
 	private String lastYearPrefix = "https://web.archive.org/web/*/";
 	private String targetYearPrefix = "https://web.archive.org/web/";
 	private String targetYearPostfix = "0101000000*/";
+	private int starYear = 2006;
+
+	private int maxLinkPerYear = 12;
+	private boolean monthGroup1 = true, monthGroup2 = true, monthGroup3 = true, monthGroup4 = true;
 
 	private String regex = "/web/([0-9]{1,}[/]{1,1})";// regular expression
 	private String targetSite;
 
-	private Map<String, String> map = new HashMap<>();
+	private Map<String, ArrayList<String>> hrefMap = new HashMap<>();
+	private ArrayList<String> hrefList = new ArrayList<>();
 
 	/**
 	 * parse target site, to get Map of unique urls.
@@ -35,7 +40,7 @@ public class ArchiveOrgSiteParser implements SiteParser, SiteParserExtend {
 
 		System.out.println("Start parsing " + targetSite + "... [Option = All YEARS{2006 - " + currentYear + "}]");
 
-		for (int i = 2006; i <= currentYear; i++) {
+		for (int i = starYear; i <= currentYear; i++) {
 			parse(i);
 		}
 
@@ -50,18 +55,19 @@ public class ArchiveOrgSiteParser implements SiteParser, SiteParserExtend {
 			return;
 		}
 
-		System.out.println("Start parsing " + targetSite + "... [Option = " + year + " YEAR]");
-
 		if (year == currentYear) {
 			parseLastYear();
 			return;
 		}
+
+		System.out.println("Start parsing " + targetSite + "... [Option = " + year + " YEAR]");
 
 		parse(targetYearPrefix + year + targetYearPostfix + targetSite);
 	}
 
 	// parse target site by last year(different page).
 	private void parseLastYear() {
+
 		System.out.println("Start parsing " + targetSite + "... [Option = LAST{" + currentYear + "} YEAR]");
 
 		parse(lastYearPrefix + targetSite);
@@ -79,10 +85,35 @@ public class ArchiveOrgSiteParser implements SiteParser, SiteParserExtend {
 			Element myBody = document.body();
 			Elements links = myBody.getElementsByAttributeValueMatching("href", Pattern.compile(regex));
 
-			for (Element link : links) {
+			ArrayList<String> newList = new ArrayList<>();
 
-				map.put(link.attr("href"), targetSite);
+			for (Element link : links) {
+				String uniqLink = "https://web.archive.org" + link.attr("href");
+
+				if (!newList.contains(uniqLink) && !hrefList.contains(uniqLink)) {
+					newList.add(uniqLink);
+				}
+
 			}
+
+			if (newList.size() > maxLinkPerYear) {
+
+				for (String string : newList) {
+					if (filter(string)) {
+
+						hrefList.add(string);
+					}
+				}
+				monthGroup1 = true;
+				monthGroup2 = true;
+				monthGroup3 = true;
+				monthGroup4 = true;
+
+			} else {
+				hrefList.addAll(newList);
+			}
+			hrefMap.put(targetSite, hrefList);
+			System.out.println(targetSite + " - " + hrefList.size());
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -94,27 +125,62 @@ public class ArchiveOrgSiteParser implements SiteParser, SiteParserExtend {
 	 * Clear map of urls
 	 */
 	public void clear() {
-		map = new HashMap<>();
+
+		hrefMap = new HashMap<>();
+		hrefList = new ArrayList<>();
+	}
+
+	private boolean filter(String string) {
+
+		// take month from url
+		int beginIndex = 32;// 9
+		int endIndex = 34;// 11
+		int manthKey = Integer.parseInt(string.substring(beginIndex, endIndex));
+
+		switch (manthKey) {
+		case 1:
+		case 2:
+		case 3:
+			if (monthGroup1) {
+				monthGroup1 = false;
+				return true;
+			}
+			return false;
+		case 4:
+		case 5:
+		case 6:
+			if (monthGroup2) {
+				monthGroup2 = false;
+				return true;
+			}
+			return false;
+		case 7:
+		case 8:
+		case 9:
+			if (monthGroup3) {
+				monthGroup3 = false;
+				return true;
+			}
+			return false;
+		case 10:
+		case 11:
+		case 12:
+			if (monthGroup4) {
+				monthGroup4 = false;
+				return true;
+			}
+			return false;
+		}
+
+		return false;
 	}
 
 	/**
-	 * Convert Map to List. completing URL
+	 * Return map URL
 	 */
-	public List<String> getTargetSiteList() {
+	public Map<String, ArrayList<String>> getTargetSiteMap() {
 
-		List<String> targetSiteList = new ArrayList<>();
-
-		if (map == null) {
-			return targetSiteList;
-		}
-
-		for (Map.Entry<String, String> entry : map.entrySet()) {
-
-			targetSiteList.add("https://web.archive.org" + entry.getKey());
-
-		}
-
-		return targetSiteList;
+		return hrefMap;
 	}
 
 	/**
@@ -122,6 +188,7 @@ public class ArchiveOrgSiteParser implements SiteParser, SiteParserExtend {
 	 */
 	public void setTargetSite(String targetSite) {
 		this.targetSite = targetSite;
+		hrefList = new ArrayList<>();
 	}
 
 	/**
