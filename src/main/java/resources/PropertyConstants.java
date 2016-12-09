ada294efc7d5b56fe73c2030d40f0e7ae34ab934
LORDexile <exilexxxx@i.ua>
2016-12-09 16:38:45 +0200

    create constants that containse neme of properties

---
+++ src/main/java/resources/PropertyConstants.java
@@ -0,0 +1,11 @@
+package resources;
+
+public final class PropertyConstants {
+	public static final String PROGRAM_VERSION = "version";
+	public static final String PATH_DRIVER = "path.driver";
+	public static final String PATH_EXPORT_FOLDER = "path.exportFolder";
+	public static final String PARSE_MAX_LINK_PER_YEAR = "parse.maxLinkPerYear";
+	public static final String PARSE_START_YEAR = "parse.starYear";
+	public static final String PARSE_MIN_TEXT_LENGHTS = "parse.minTextLenghts";
+
+}
