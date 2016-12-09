2cf45652c2e333598c3c2459a48f8d33a2a26a96
LORDexile <exilexxxx@i.ua>
2016-12-09 16:44:27 +0200

    ver 0.1.4
    add tmp folder to program source

--- src/main/java/resources/Constants.java
+++ src/main/java/resources/Constants.java
@@ -5,10 +5,11 @@ import java.io.File;
 import javax.swing.filechooser.FileSystemView;
 
 public final class Constants {
-	public static final String PRODUCT_VERSION = "0.1.3";
+	public static final String PRODUCT_VERSION = "0.1.4";
 	public static final String PROGRAM_NAME = "ScreenShotSaver";
 	public static final String PROGRAM_TITLE = PROGRAM_NAME + " ver: [" + PRODUCT_VERSION + "]";
 	public static final String PROGRAM_SOURCE_FOLDER = FileSystemView.getFileSystemView().getDefaultDirectory() + File.separator + PROGRAM_NAME;
 	public static final String CONFIG_PATH = PROGRAM_SOURCE_FOLDER + File.separator + "config.properties";
+	public static final String TMP_PATH = PROGRAM_SOURCE_FOLDER + File.separator + "tmp";
 
 }
