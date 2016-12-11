85ee2c30c00a6ccf0266675c0b056a37b132785e
LORDexile <exilexxxx@i.ua>
2016-12-11 19:11:10 +0200

    add path that contains URL for uniq test

--- src/main/java/resources/Constants.java
+++ src/main/java/resources/Constants.java
@@ -5,11 +5,12 @@ import java.io.File;
 import javax.swing.filechooser.FileSystemView;
 
 public final class Constants {
-	public static final String PRODUCT_VERSION = "0.1.4.1";
+	public static final String PRODUCT_VERSION = "0.1.4.2";
 	public static final String PROGRAM_NAME = "ScreenShotSaver";
 	public static final String PROGRAM_TITLE = PROGRAM_NAME + " ver: [" + PRODUCT_VERSION + "]";
 	public static final String PROGRAM_SOURCE_FOLDER = FileSystemView.getFileSystemView().getDefaultDirectory() + File.separator + PROGRAM_NAME;
 	public static final String CONFIG_PATH = PROGRAM_SOURCE_FOLDER + File.separator + "config.properties";
 	public static final String TMP_PATH = PROGRAM_SOURCE_FOLDER + File.separator + "tmp";
 
+	public static final String UNIQUE_TEST_URL_PATH = "https://text.ru/antiplagiat";
 }
